9294f42868aa7f3349ac6d79a5822c94a1e641c8
coachO88 <rangel52088@gmail.com>
2019-04-27 10:27:38 -0700

    Updateds to html files

--- FriendFolder/app/data/friend.js
+++ FriendFolder/app/data/friend.js
@@ -16,7 +16,7 @@ var friend = [
        ]
      },
      {  
-       "name":"Person2",
+       "name":"Orlando Rangel",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
             2,
--- FriendFolder/server.js
+++ FriendFolder/server.js
@@ -14,20 +14,9 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiRoute")(app);
 require("./app/routing/htmlRoute")(app);
 
-// app.post('/api/friends', function(req, res) {
-//     res.send(req.body);
-// })
-
 app.get('/api/friend', function(req, res) {
     res.json(friend);
-    console.log('55555');
-  });
-// app.use(express.static("app/public"));
-// app.use(express.static("app/data"));
-
-// require("./app/routing/htmlRoutes")(app);
-// require("./app/routing/apiRoutes")(app);
-
-
+    // console.log('55555');
+});
 
 app.listen(port, () => console.log("Listening on port %s", port));
